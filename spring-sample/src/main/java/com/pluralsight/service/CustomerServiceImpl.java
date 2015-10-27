6a48e3a2583349fd2c4a447d867d86640c7551ea
tykowale <tykowale@gmail.com>
2015-10-27 18:45:22 -0500

    java constructor configuration

--- spring-sample/src/main/java/com/pluralsight/service/CustomerServiceImpl.java
+++ spring-sample/src/main/java/com/pluralsight/service/CustomerServiceImpl.java
@@ -14,6 +14,14 @@ public class CustomerServiceImpl implements CustomerService{
 
     private CustomerRepository customerRepository;
 
+    public CustomerServiceImpl() {
+
+    }
+
+    public CustomerServiceImpl(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
     public void setCustomerRepository(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
     }
