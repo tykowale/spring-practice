7fa6f2afc28f3209c99c71589b3050f1c16a99cf
tykowale <tykowale@gmail.com>
2015-10-26 21:54:22 -0500

    spring annotation configuration

--- spring-sample/src/main/java/com/pluralsight/repository/HibernateCustomerRepositoryImpl.java
+++ spring-sample/src/main/java/com/pluralsight/repository/HibernateCustomerRepositoryImpl.java
@@ -1,6 +1,7 @@
 package com.pluralsight.repository;
 
 import com.pluralsight.model.Customer;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,6 +9,7 @@ import java.util.List;
 /**
  * Created by tylerkowalewski on 10/25/15.
  */
+@Repository("customerRepository")
 public class HibernateCustomerRepositoryImpl implements CustomerRepository{
     public List<Customer> findAll() {
         List<Customer> customers = new ArrayList<>();
--- spring-sample/src/main/java/com/pluralsight/service/CustomerServiceImpl.java
+++ spring-sample/src/main/java/com/pluralsight/service/CustomerServiceImpl.java
@@ -2,19 +2,20 @@ package com.pluralsight.service;
 
 import com.pluralsight.model.Customer;
 import com.pluralsight.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 /**
  * Created by tylerkowalewski on 10/25/15.
  */
+@Service("customerService")
 public class CustomerServiceImpl implements CustomerService{
+
     private CustomerRepository customerRepository;
 
-    public CustomerServiceImpl() {
-
-    }
-
+    @Autowired
     public void setCustomerRepository(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
     }
